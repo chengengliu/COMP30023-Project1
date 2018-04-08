58b94a8374acb6b5d2add7afb1e30274fd5f326a
VictorLiu <victorleow@outlook.com>
2018-04-08 16:17:38 +1000

    Add three files

---
+++ get_file_by_name.c
@@ -0,0 +1,11 @@
+#include "my_server.h"
+
+int get_file_by_name(){
+  DIR *dir;
+  struct dirent *d;
+  directory = opendir("./test");
+  //No need to check if the path is correct or not
+  //Default as existent
+  while(d= readdir(directory)!=NULL){
+    if()
+  }
--- my_server.c
+++ my_server.c
@@ -1,6 +1,5 @@
 #include "my_server.h"
 
 int main(int argc, char const *argv[]) {
-  //printf("Size of the size_t is %d", sizeof(size_t));
-  return 0;
+
 }
